f1b83ef199eb9c862dbdde05e49d167bc4494676
Diogo Paza <diogopazacvel@gmail.com>
2018-10-24 08:12:11 -0300

    start 24/10

--- percorrendo_linhas_colunas_imagem.py
+++ percorrendo_linhas_colunas_imagem.py
@@ -1,6 +1,7 @@
 import cv2
 
 imagem = cv2.imread( "jogador.jpg ")
-for y in range( 0, imagem.shape[0], 1)
-
+for y in range( 0, imagem.shape[0], 1):#percorre linhas
+   for x in range( 0, imagem.shape[1],1):#percorre colunas
+       imagem[x, y ]= (255,255,255)#deixa imagem em branco
 cv2.imshow('input', imagem)
